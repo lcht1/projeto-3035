22097b62c471ea869822b8fefc21411eb6c52af6
Manuela Leichtveis <manuleichtveis@gmail.com>
2021-10-21 19:21:55 -0300

    nav toggle

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+    element.addEventListener('click', () => {
+        nav.classList.toggle('show')
+    })
+}
+
+const links = document.querySelectorAll('nav ul li a')
+for (const link of links) {
+    link.addEventListener('click', () => {
+        nav.classList.remove('show')
+    })
+}
+
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
 function changeHeader() {
-  if (window.scrollY >= navHeight) {
-    header.classList.add('scroll')
-  } else {
-    header.classList.remove('scroll')
-  }
+    if (window.scrollY >= navHeight) {
+        header.classList.add('scroll')
+    } else {
+        header.classList.remove('scroll')
+    }
 }
 
 const buttonBack = document.querySelector('.back-to-top')
